a28279b24e42fe1453c2aa400ed25d80cf21e568
sharkpp <touroku@sharkpp.net>
2016-06-11 12:02:52 +0900

    テストその４

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,10 @@ git config --global user.name  "sharkpp"
 
 git clone --quiet -b gh-pages "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" output_prod
 pushd output_prod
-git clone remote
-git fetch
-git rebase origin/gh-pages
+git remote
+git pull
+#git fetch
+#git rebase origin/gh-pages
 popd
 rm -rf output_prod/*
 
@@ -22,7 +23,8 @@ pushd output_prod
 
 git add . -A
 git commit -m "Deploy $(date '+%Y%m%d%H%M%S') to GitHub Pages"
-git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+#git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+git push
 
 popd
 
@@ -35,8 +37,7 @@ cat .tw.yml | sed \
     -e "s/TW_USER_NAME/${TW_USER_NAME}/g" \
     > ~/.tw.yml
 
-which tw
-
-ls -la ~/
+#which tw
+#ls -la ~/
 
 # echo -e "てすと $(date '+%Y%m%d%H%M%S')\n https://travis-ci.org/sharkpp/travis-ci-pull-test" | tw --pipe
